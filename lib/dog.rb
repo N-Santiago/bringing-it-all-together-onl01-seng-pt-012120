8d9e9a5bbecba5c42dd19f3f176224766d7995ed
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-28 02:25:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,7 +10,7 @@ class Dog
   
   def self.create_table
     sql = "CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT"
-    DB[:conn].execute
+    DB[:conn].execute(sql)
   end
   
 end 
