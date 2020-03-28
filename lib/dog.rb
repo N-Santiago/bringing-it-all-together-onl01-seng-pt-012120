779a9a896dbd9a75df63aa5d535579bbb4987d15
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-28 03:20:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,8 +39,8 @@ class Dog
   
   def self.new_from_db(row)
     dogs_hash = {
-    :id => row[0]
-    :name => row[1]
+    :id => row[0],
+    :name => row[1],
     :breed => row[2]
     }
     self.new(dogs_hash)
