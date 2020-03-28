cff44ea57a2cfcac81076ff68c080c06d0f71ebd
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-28 02:34:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,4 +18,15 @@ class Dog
     DB[:conn].execute(sql)
   end 
   
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+ 
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
+  end
+  
 end 
